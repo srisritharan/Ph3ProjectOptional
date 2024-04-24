1326aac2c08efc3d7eb201e017afc1b1f71b165b
Sritharan_Srinivasan <srisritharan.s@gmail.com>
2024-04-24 18:05:53 -0400

    code formated

--- data-access.js
+++ data-access.js
@@ -10,94 +10,99 @@ let collection;
 //Connect to mongodb
 
 async function dbConnect() {
-  try {
-    const client = new MongoClient(connectString);
-    await client.connect();
-    collection = client.db(dbName).collection(collectionName);
-    console.log(
-      new Date().toLocaleString() +
-        ` DB Connected ${connectString} Collection ${collectionName}`
-    );
-  } catch (err) {
-    console.log(err.message);
-    console.log("DB CONNECTION FAILED. Is database running?");
-  }
+    try {
+        const client = new MongoClient(connectString);
+        await client.connect();
+        collection = client.db(dbName).collection(collectionName);
+        console.log(
+            new Date().toLocaleString() +
+            ` DB Connected ${connectString} Collection ${collectionName}`
+        );
+    } catch (err) {
+        console.log(err.message);
+        console.log("DB CONNECTION FAILED. Is database running?");
+    }
 }
 
 // get Customers
 module.exports.getCustomers = async function () {
-  try {
-    const customers = await collection.find().toArray();
-    console.log(new Date().toLocaleString() + " getCustomers");
-    // throw {"message":"an error occured"};
-    return [customers, null];
-  } catch (err) {
-    console.log(err.message);
-    return [null, err.message];
-  }
+    try {
+        const customers = await collection.find().toArray();
+        console.log(new Date().toLocaleString() + " getCustomers");
+        // throw {"message":"an error occured"};
+        return [customers, null];
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
 };
 
 // reset Customers
 module.exports.resetCustomers = async function () {
-  let cusData = [
-    { id: 0, name: "Mary Jackson", email: "maryj@abc.com", password: "maryj" },
-    {
-      id: 1,
-      name: "Karen Addams",
-      email: "karena@abc.com",
-      password: "karena",
-    },
-    {
-      id: 2,
-      name: "Scott Ramsey",
-      email: "scottr@abc.com",
-      password: "scottr",
-    },
-  ];
-  try {
-    await collection.deleteMany({});
-    await collection.insertMany(cusData);
-    const customers = await collection.find().toArray();
-    const msg =
-      "New data loaded at " +
-      new Date().toLocaleString() +
-      " <br/> There are now " +
-      customers.length +
-      " customer records!";
-    console.log(new Date().toLocaleString() + " resetCustomers");
-    return [msg, null];
-  } catch (err) {
-    console.log(err.message);
-    return [null, err.message];
-  }
+    let cusData = [
+      {
+        id: 0,
+        name: "Mary Jackson",
+        email: "maryj@abc.com",
+        password: "maryj",
+      },
+      {
+        id: 1,
+        name: "Karen Addams",
+        email: "karena@abc.com",
+        password: "karena",
+      },
+      {
+        id: 2,
+        name: "Scott Ramsey",
+        email: "scottr@abc.com",
+        password: "scottr",
+      },
+    ];
+    try {
+        await collection.deleteMany({});
+        await collection.insertMany(cusData);
+        const customers = await collection.find().toArray();
+        const msg =
+            "New data loaded at " +
+            new Date().toLocaleString() +
+            " <br/> There are now " +
+            customers.length +
+            " customer records!";
+        console.log(new Date().toLocaleString() + " resetCustomers");
+        return [msg, null];
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
 };
 
 // add Customer
 module.exports.addCustomer = async function (newCustomer) {
-  try {
-    const insertResult = await collection.insertOne(newCustomer);
-    // return array [status, id, errMessage]
-    console.log(new Date().toLocaleString() + " addCustomer");
-    return ["success", insertResult.insertedId, null];
-  } catch (err) {
-    console.log(err.message);
-    return ["fail", null, err.message];
-  }
+    try {
+        const insertResult = await collection.insertOne(newCustomer);
+        // return array [status, id, errMessage]
+        console.log(new Date().toLocaleString() + " addCustomer");
+        return ["success", insertResult.insertedId, null];
+    } catch (err) {
+        console.log(err.message);
+        return ["fail", null, err.message];
+    }
 };
 
 // get Customer By Id
 module.exports.getCustomerById = async function (id) {
     try {
-      const customer = await collection.findOne({"id": +id});
-      console.log(new Date().toLocaleString() + " getCustomerById");
-      if(!customer){
-        return [null, "Invalid Customer Number </br> Customer not found (404)!"];  
-      }
-      return [customer, null];
+        const customer = await collection.findOne({ "id": +id });
+        console.log(new Date().toLocaleString() + " getCustomerById");
+        if (!customer) {
+            return [null, "Invalid Customer Number </br> Customer not found (404)!"];
+        }
+        return [customer, null];
     } catch (err) {
-      console.log(err.message);
-      return [null, err.message];
+        console.log(err.message);
+        return [null, err.message];
     }
-  };
-  
+};
+
 dbConnect();
--- server.js
+++ server.js
@@ -14,50 +14,50 @@ app.use(express.static(staticDir));
 
 //start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-  console.log("staticDir: " + staticDir);
+    console.log(`Server listening on port ${port}`);
+    console.log("staticDir: " + staticDir);
 });
 
 //getCustomers
 app.get("/customers", async (req, res) => {
-  const [cust, err] = await da.getCustomers();
-  if (cust) {
-    res.send(cust);
-  } else {
-    res.status(500).send(err);
-  }
+    const [cust, err] = await da.getCustomers();
+    if (cust) {
+        res.send(cust);
+    } else {
+        res.status(500).send(err);
+    }
 });
 
 // reset Customers
 app.get("/reset", async (req, res) => {
-  const [result, err] = await da.resetCustomers();
-  if (result) {
-    res.send(result);
-  } else {
-    res.status(500).send(err);
-  }
+    const [result, err] = await da.resetCustomers();
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(500).send(err);
+    }
 });
 
 // adding a new customer
 app.post("/customers", async (req, res) => {
-  const newCustomer = req.body;
-  // Check if the request body is missing
-  if (Object.keys(req.body).length === 0) {
-    res.status(400).send("Missing Request body");
-  } else {
-    // Check if the required properties are present
-    if (!newCustomer.name || !newCustomer.email || !newCustomer.password) {
-      res.status(400).send("Missing Required Feilds");
-      return;
-    }
-    // Process the request
-    const [status, id, errMessage] = await da.addCustomer(newCustomer);
-    if (status === "success") {
-      res.status(201).send({ ...newCustomer, _id: id });
+    const newCustomer = req.body;
+    // Check if the request body is missing
+    if (Object.keys(req.body).length === 0) {
+        res.status(400).send("Missing Request body");
     } else {
-      res.status(400).send(errMessage);
+        // Check if the required properties are present
+        if (!newCustomer.name || !newCustomer.email || !newCustomer.password) {
+            res.status(400).send("Missing Required Feilds");
+            return;
+        }
+        // Process the request
+        const [status, id, errMessage] = await da.addCustomer(newCustomer);
+        if (status === "success") {
+            res.status(201).send({ ...newCustomer, _id: id });
+        } else {
+            res.status(400).send(errMessage);
+        }
     }
-  }
 });
 
 //getCustomerByID
@@ -65,8 +65,8 @@ app.get("/customers/:id", async (req, res) => {
     const id = req.params.id;
     const [cust, err] = await da.getCustomerById(id);
     if (cust) {
-      res.send(cust);
+        res.send(cust);
     } else {
-      res.status(404).send(err);
+        res.status(404).send(err);
     }
-  });
+});
